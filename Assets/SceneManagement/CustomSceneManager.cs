9473fbfcee7bfb99d5683f7bd892c7be4daa856d
Louis Ashton <louisjamesashton@gmail.com>
2026-08-25 20:24:04 +0100

    Imported singletons

---
+++ Assets/SceneManagement/CustomSceneManager.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using LouisAshton.Singletons;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomSceneManager : PersistentSingleton<CustomSceneManager>
+{
+
+}
