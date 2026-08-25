6e542c9ae365242d223ef03d353d4bc641d086e6
Louis Ashton <louisjamesashton@gmail.com>
2026-08-25 15:17:14 +0100

    Written camera movement script

---
+++ Assets/Camera/CameraLerp.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLerp : MonoBehaviour
+{
+    [SerializeField] private List<Transform> positions;
+    [SerializeField, Min(0.1f)] private float transitionLength; 
+    
+    private int _targetTransformIndex = 0;
+
+    private float _lastTime = 0;
+    private Vector3 _lastPosition;
+    private Quaternion _lastRotation;
+    
+    private void Awake()
+    {
+        _lastPosition = transform.position;
+        _lastRotation = transform.rotation;
+        _lastTime = Time.time;
+    }
+
+    public void SetTarget(int index)
+    {
+        _lastPosition = transform.position;
+        _lastRotation = transform.rotation;
+        _lastTime = Time.time;
+        _targetTransformIndex = index;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            _targetTransformIndex += (_targetTransformIndex + 1) % positions.Count;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_targetTransformIndex >= positions.Count)
+            return;
+        
+        var t = (Time.time - _lastTime) / transitionLength;
+        
+        transform.position = Vector3.Lerp(_lastPosition, positions[_targetTransformIndex].position, t);
+        transform.rotation = Quaternion.Lerp(_lastRotation, positions[_targetTransformIndex].rotation, t);
+    }
+}
