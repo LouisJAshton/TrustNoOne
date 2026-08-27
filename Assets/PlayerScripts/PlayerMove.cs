1dd7c96d89b13a1eae0c4d879e8c319510c26faa
Louis Ashton <louisjamesashton@gmail.com>
2026-08-27 22:07:47 +0100

    Made movement snappier

--- Assets/PlayerScripts/PlayerMove.cs
+++ Assets/PlayerScripts/PlayerMove.cs
@@ -40,14 +40,14 @@ public class PlayerMove : MonoBehaviour
         if (disableMovement == 0 && MoveAction.WasPerformedThisFrame())
         {
             Debug.Log("MOVED");
-            disableMovement = 22;
+            disableMovement = 10;
             NewPos = transform.position + transform.forward*4.5f;
             move = true;
         }
         else if (disableMovement == 0 && RotateLAction.WasPerformedThisFrame())
         {
             Debug.Log("ROTATE:L");
-            disableMovement = 33;
+            disableMovement = 10;
             NewRot = currentRot.eulerAngles;
             NewRot.y = NewRot.y - 90;
             rotate = true;
@@ -55,7 +55,7 @@ public class PlayerMove : MonoBehaviour
         else if (disableMovement == 0 && RotateRAction.WasPerformedThisFrame())
         {
             Debug.Log("ROTATE:R");
-            disableMovement = 33;
+            disableMovement = 10;
             NewRot = currentRot.eulerAngles;
             NewRot.y = NewRot.y + 90;
             rotate = true;
