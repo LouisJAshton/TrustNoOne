ee1a4366456bfc37beddecdc2b3f98386e2223ae
Louis Ashton <louisjamesashton@gmail.com>
2026-08-29 17:59:38 +0100

    locked z and x rots for billboards

--- Assets/Camera/CameraBillboarder.cs
+++ Assets/Camera/CameraBillboarder.cs
@@ -13,7 +13,12 @@ public class CameraBillboarder : MonoBehaviour
 
     private void Update()
     {
-        if(_camera)
+        if (_camera) {
             transform.LookAt(_camera.transform);
+            var rot = transform.rotation.eulerAngles;
+            rot.x = 0;
+            rot.z = 0;
+            transform.rotation = Quaternion.Euler(rot);
+        }
     }
 }
