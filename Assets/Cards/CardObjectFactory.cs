2f507eb34b2792a840047fc4d16abbbcbc1e7f0d
Louis Ashton <louisjamesashton@gmail.com>
2026-08-29 11:48:16 +0100

    Instanced each card material

--- Assets/Cards/CardObjectFactory.cs
+++ Assets/Cards/CardObjectFactory.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "CardObjectFactory", menuName = "Scriptable Objects/CardObjectFactory")]
 public class CardObjectFactory : ScriptableObject
 {
+    private static readonly int TimeOffset = Shader.PropertyToID("_TimeOffset");
     [SerializeField] private CardObject cardObjectPrefab;
     
     public CardObject Create(CardInfo cardInfo)
@@ -14,6 +16,11 @@ public class CardObjectFactory : ScriptableObject
         
         var cardObject = Instantiate(cardObjectPrefab);
         cardObject.SetCardInfo(cardInfo);
+
+        if (cardObject.TryGetComponent<RawImage>(out var image)) {
+            image.material = new Material(image.material);
+            image.material.SetFloat(TimeOffset, Random.Range(0, 100000));
+        }
         
         return cardObject;
     }
